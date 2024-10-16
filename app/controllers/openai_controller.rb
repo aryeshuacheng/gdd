c940e818b865005eebbf3409e25f6e8126ed6909
Illuminus <aycheng8@terpmail.umd.edu>
2024-10-16 13:05:51 -0400

    Made the API call asynch

--- app/controllers/openai_controller.rb
+++ app/controllers/openai_controller.rb
@@ -2,7 +2,10 @@ class OpenaiController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   def get_response
-    @trained_response = OpenaiService.new(params[:query], params[:type]).call
+
+    Thread.new{@trained_response = OpenaiService.new(params[:query], params[:type]).call}
+
+    redirect_to :controller => 'proposals', :action => 'index'
     # @untrained_response = OpenaiService.new(params[:query], params[:type]).call_untrained
   end
 end
